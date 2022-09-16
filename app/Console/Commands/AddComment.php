49d18ffd5a1edd0e2993f9d21b3b02db0a496442
Your Name <you@example.com>
2022-09-17 02:56:17 +0800

    adding custom artisan command function

---
+++ app/Console/Commands/AddComment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+
+class AddComment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:comment {id} {comment}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add comment to the user basing on the user id';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $request = new Request();
+        $values = [
+            'id' => $this->argument('id'), 
+            'comments' => $this->argument('comment');
+        $request->merge($values);
+        app(UserController::class)->add_comment($request);
+    }
+}
