cb98256d8787772b538e1bbb44f3e01b3005bb43
Your Name <you@example.com>
2022-09-16 22:49:03 +0800

    adding userseeder for generating random users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(rand(5,10)).' '.Str::random(rand(8,13)),
+            'comments' => Str::random(50)
+        ]);
+    }
+}
